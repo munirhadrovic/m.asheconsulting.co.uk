ba0fef544a9e0998f5801ce38b5b53f85e2d6bfe
novicaa <novicamiketic@gmail.com>
2015-04-16 14:33:33 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="28a East Street, Horsham, RH12 1HL"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Florida 34104";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=01403254079;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
